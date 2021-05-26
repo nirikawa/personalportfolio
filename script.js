8ac308146599b4df07db2a17cbc91bf671aa45a8
Nick <nirikawadev@gmail.com>
2021-05-26 01:04:27 -0700

    Started working on night mode

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ panels.forEach((panel) => {
 function removeActiveClasses() {
     document.querySelector('.active').classList.remove("active");
 }
+
+// Night Mode START
+let checkBox = document.getElementById("night-toggle");
+checkBox.addEventListener('change', event => {
+  if(checkBox.checked == true){
+    // night mode is on
+    // add night class
+    document.querySelectorAll(".canEdit").forEach(item => {
+      item.classList.remove('disabled');
+    });
+  }
+  else {
+    document.querySelectorAll(".canEdit").forEach(item => {
+      item.classList.add('disabled');
+    });
+  }
+});
+// Night Mode END
